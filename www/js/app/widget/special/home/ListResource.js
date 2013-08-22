c1182f5ae87448abc21a5c6c47a69d4a09e6482a
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-08-22 15:26:07 +0800

    test

--- www/js/app/widget/special/home/ListResource.js
+++ www/js/app/widget/special/home/ListResource.js
@@ -8,23 +8,20 @@ define([
 ], function (declare, lang, array, registry, RoundRectStoreList, StoredData) {
     return declare("app.widget.special.home.ListResource", [RoundRectStoreList, StoredData], {
         resourceMonitorId: null,
-        appendMessage: function (label, message) {
+        appendMessage: function (message) {
             if (typeof message == "undefined" && (typeof message == "string" || message.constructor == String)) {
-                this.store.put({ "id": this.id + "_" + (this.data.length + 1), "label": label, "rightText": message.replace(/\n/g, "<br />"), "variableHeight": true });
+                this.store.put({ "id": this.id + "_" + (this.data.length + 1), "label": message.replace(/\n/g, "<br />"), "variableHeight": true });
             }
             else {
-                this.store.put({ "id": this.id + "_" + (this.data.length + 1), "label": label, "rightText": message, "variableHeight": true });
+                this.store.put({ "id": this.id + "_" + (this.data.length + 1), "label": message, "variableHeight": true });
             }
-        },
-        logMessage: function (data) {
-            this.appendMessage("System", data.message);
         },
         theyAre: function (who) {
             array.forEach(this.store.query({}), lang.hitch(this, function (item, index) {
                 this.store.remove(item.id);
             }));
             array.forEach(who, lang.hitch(this, function (item, index) {
-                this.appendMessage("they.are", item.id);
+                this.appendMessage(item.id);
             }));
         },
         postCreate: function () {
