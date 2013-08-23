615b1bb00821143319bbde5326c88a4a90cfd044
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-08-23 15:58:52 +0800

    test

--- www/js/app/widget/special/home/BtnTellOther.js
+++ www/js/app/widget/special/home/BtnTellOther.js
@@ -3,19 +3,23 @@ define([
     "dojo/_base/lang",
     "dojo/on",
     "dojo/topic",
+    "dijit/registry",
     "dojox/mobile/Button"
-], function (declare, lang, on, topic, Button) {
+], function (declare, lang, on, topic, registry, Button) {
     return declare("app.widget.special.home.BtnTellOther", [Button], {
+        textId: null,
         postCreate: function () {
             this.inherited(arguments);
 
-            on(this, "click", lang.hitch(this, function (e) {
-                if (e != null) {
-                    e.preventDefault();
-                }
+            if (this.textId != null) {
+                on(this, "click", lang.hitch(this, function (e) {
+                    if (e != null) {
+                        e.preventDefault();
+                    }
 
-                topic.publish("/resourceMonitor/tell.other", "I am fine.");
-            }));
+                    topic.publish("/resourceMonitor/tell.other", registry.byId(this.textId).get("value"));
+                }));
+            }
         }
     });
 });
