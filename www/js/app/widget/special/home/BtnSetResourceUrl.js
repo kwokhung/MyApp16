f30f20a459590bd1e589769f8ee203aee55f6110
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-09-09 09:59:16 +0800

    test

--- www/js/app/widget/special/home/BtnSetResourceUrl.js
+++ www/js/app/widget/special/home/BtnSetResourceUrl.js
@@ -7,17 +7,17 @@ define([
     "dojox/mobile/Button"
 ], function (declare, lang, on, topic, registry, Button) {
     return declare("app.widget.special.home.BtnSetResourceUrl", [Button], {
-        textId: null,
+        resourceUrlId: null,
         postCreate: function () {
             this.inherited(arguments);
 
-            if (this.textId != null) {
+            if (this.resourceUrlId != null) {
                 on(this, "click", lang.hitch(this, function (e) {
                     if (e != null) {
                         e.preventDefault();
                     }
 
-                    topic.publish("/resourceMonitor/set.resource.url", registry.byId(this.textId).get("value"));
+                    topic.publish("/resourceMonitor/set.resource.url", registry.byId(this.resourceUrlId).get("value"));
                 }));
             }
         }
