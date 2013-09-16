b4331bc6fdbc8c74e860d327483ae23ee21b5eb4
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-09-16 14:28:10 +0800

    test

--- www/js/app/widget/special/home/BtnTellSomeone.js
+++ www/js/app/widget/special/home/BtnTellSomeone.js
@@ -3,27 +3,41 @@ define([
     "dojo/_base/lang",
     "dojo/on",
     "dojo/topic",
-    "dijit/registry",
-    "dojox/mobile/Button"
-], function (declare, lang, on, topic, registry, Button) {
-    return declare("app.widget.special.home.BtnTellSomeone", [Button], {
-        whomId: null,
-        whatId: null,
+    "dojox/mobile/Button",
+    "app/widget/_Subscriber"
+], function (declare, lang, on, topic, Button, _Subscriber) {
+    return declare("app.widget.special.home.BtnTellSomeone", [Button, _Subscriber], {
+        whom: null,
+        what: null,
+        setWhomTopicId: null,
+        setWhatTopicId: null,
+        setWhom: function (data) {
+            this.whom = data.newValue;
+        },
+        setWhat: function (data) {
+            this.what = data.newValue;
+        },
         postCreate: function () {
             this.inherited(arguments);
 
-            if (this.whomId != null && this.whatId != null) {
-                on(this, "click", lang.hitch(this, function (e) {
-                    if (e != null) {
-                        e.preventDefault();
-                    }
-
-                    topic.publish("/resourceMonitor/tell.someone", {
-                        whom: registry.byId(this.whomId).get("value"),
-                        what: registry.byId(this.whatId).get("value")
-                    });
-                }));
+            if (this.setWhomTopicId != null) {
+                this.subscribers.push(topic.subscribe(this.setWhomTopicId, lang.hitch(this, this.setWhom)));
             }
+
+            if (this.setWhatTopicId != null) {
+                this.subscribers.push(topic.subscribe(this.setWhatTopicId, lang.hitch(this, this.setWhat)));
+            }
+
+            on(this, "click", lang.hitch(this, function (e) {
+                if (e != null) {
+                    e.preventDefault();
+                }
+
+                topic.publish("/resourceMonitor/tell.someone", {
+                    whom: this.whom,
+                    what: this.what
+                });
+            }));
         }
     });
 });
