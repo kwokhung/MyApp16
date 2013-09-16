5383f0e225de0f479763ed1e25fe61cbcfa14bfc
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-09-16 10:35:24 +0800

    test

--- www/js/app/widget/_Valuable.js
+++ www/js/app/widget/_Valuable.js
@@ -7,6 +7,7 @@ define([
 ], function (declare, lang, on, topic, _Subscriber) {
     return declare("app.widget._Valuable", [_Subscriber], {
         setValueTopicId: null,
+        topicId: null,
         setValue: function (data) {
             this.set("value", data.newValue);
         },
@@ -15,6 +16,14 @@ define([
 
             if (this.setValueTopicId != null) {
                 this.subscribers.push(topic.subscribe(this.setValueTopicId, lang.hitch(this, this.setValue)));
+            }
+
+            if (this.topicId != null) {
+                on(this, "change", lang.hitch(this, function (newValue) {
+                    topic.publish(this.topicId, {
+                        newValue: newValue
+                    });
+                }));
             }
         }
     });
